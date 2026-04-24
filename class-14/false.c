218f1141f3e14ee689016b9c9580554b8ec0c1fd
Miftahul Islam <almiftahulislam@gmail.com>
2026-04-24 22:28:18 +0600

    add: Class 14

---
+++ class-14/false.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", 10 > 0); // true
+    printf("%d\n", 10 == 0);
+    printf("%d\n", 10 == 10); // true
+    printf("%d\n", 10 != 10);
+    printf("%d\n", 10 < 10);
+
+    if ("sasdsad")
+    {
+        printf("True\n");
+    }
+
+    return 0;
+}
--- class-14/readme.md
+++ class-14/readme.md
@@ -1,1 +1,4 @@
 # Class - 14
+
+# True and False in C Programming
+- C-তে, একটি expression true হয় যদি এটি evaluate হয়ে যেকোনো nonzero value (শূন্য ছাড়া অন্য কোনো মান) হয়। যদি এটি evaluate হয়ে zero (শূন্য) হয়, তবে এটি false। 
